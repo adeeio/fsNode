a83138e316d9be280e02b83cd6c2a61a8b8c05a6
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-29 20:47:13 +0100

    badamy otoczenie completed

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 
 var fs = require('fs');
-var colors = require('colors');
 
-fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-    console.log('Dane przed zapisem!' .blue);
-    console.log(data);
 
-    fs.appendFile('./tekst.txt', 'Mordolencja!', function(err) {
+fs.readdir('C:/Users/Adrian Ostapkiewicz/Desktop/godzilla/fsNode/', 'utf-8', function(err, files){
+    console.log(files);
+
+    fs.writeFile('dirContent.txt', files, 'utf-8', function(err){
         if (err) throw err;
-        console.log('Zapisano!' .blue);
-
-        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-            console.log('Dane po zapisie' .blue)
-            console.log(data);
-        });
     });
 });
 
