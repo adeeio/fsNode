11e1138423169a87f5c4dd8516dc9c62acc8be25
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-30 19:11:36 +0100

    updated index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 var fs = require('fs');
 
 
-fs.readdir('C:/Users/Adrian Ostapkiewicz/Desktop/godzilla/fsNode/', 'utf-8', function(err, files){
-    console.log(files);
+fs.readdir('./', 'utf-8', function(err, files){
 
     fs.writeFile('dirContent.txt', files, 'utf-8', function(err){
         if (err) throw err;
